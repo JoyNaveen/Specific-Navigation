71edafb54992246034180aaf4ee9ab52c06efc65
JoyNaveen <naveenjoy975@gmail.com>
2023-03-16 19:46:18 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def joy(request):
+    return render(request,'joy.html')
---
+++ app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def joy2(request):
+    return render(request,'joy2.html')
